a2fd91835e23970bcbcb76258ab561b741318656
Dan <me@danielholcomb.com>
2021-10-05 21:11:47 -0600

    Basic Movement and Base Model Added

---
+++ Assets/Scenes/Actor.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpAmount;
+
+    CharacterController cc;
+    Animator anim;
+    float movement;
+    Vector3 moveDirection;
+    float jumpMovement;
+    float lastX = -1;
+    
+    
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        moveDirection.y -= 9.8f * Time.deltaTime;
+        
+        if(cc.isGrounded)
+        {
+            moveDirection.y = jumpMovement;
+        }
+
+        moveDirection.x = movement;
+
+        moveDirection.x *= speed;
+
+        anim.SetFloat("MoveSpeed", Mathf.Abs(moveDirection.x));
+
+        transform.rotation = Quaternion.Euler(0f, lastX * 90f, 0f);
+
+        cc.Move(moveDirection * Time.deltaTime);
+
+    }
+
+    public void Move(Vector2 input)
+    {
+        movement = input.x;
+        if(input.x != 0)
+            lastX = Mathf.Sign(input.x);
+    }
+
+    public void Jump(bool start)
+    {
+        if(start)
+        {
+            jumpMovement = jumpAmount;
+            anim.SetBool("jumped", true);
+        }
+        else
+        {
+            anim.SetBool("jumped", false);
+            jumpMovement = 0;
+        }
+    }
+
+    public void Attack()
+    {
+    }
+}
---
+++ Assets/Scenes/InputManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] Actor player;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            player.Move(context.ReadValue<Vector2>());
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.started || context.performed)
+        {
+            player.Jump(true);
+        }
+        else
+        {
+            player.Jump(false);
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            player.Attack();
+        }
+    }
+}
